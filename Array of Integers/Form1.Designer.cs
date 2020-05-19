dab2925f303b4e3b4d00ad67e0830128f610cb55
Arslan Akhtar <19163@bayfield-high.school.nz>
2020-05-20 11:48:23 +1200

    Add project files.

---
+++ Array of Integers/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Array_of_Integers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblOutput2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(99, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(107, 57);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(315, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Find Words";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblOutput2
+            // 
+            this.lblOutput2.AutoSize = true;
+            this.lblOutput2.Location = new System.Drawing.Point(333, 57);
+            this.lblOutput2.Name = "lblOutput2";
+            this.lblOutput2.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 261);
+            this.Controls.Add(this.lblOutput2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblOutput2;
+    }
+}
+
---
+++ Array of Integers/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Array_of_Integers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] nums = { 8, 9, 5, 6, 2, 8, 4 };
+            int total;
+            for (int i=0;i<nums.Length;i++)
+            
+            {
+                lblOutput.Text = "The first element is " + nums [0];
+                lblOutput.Text += "\r\n" + "The second element is " + nums[4];
+                
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] Fruit = { "Banana", "Apple", "Pear", "Orange", "Kiwi fruit" };
+            Fruit[1] = "Stwarberry";
+            int numoffruits;
+            numoffruits = Fruit.Length;
+            {
+                lblOutput2.Text = Fruit[1] + " is the second element";
+                lblOutput2.Text += "\r\n" + "Total Fruit is " + numoffruits;
+            }
+        }
+    }
+}
